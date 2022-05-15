e9dca995053c99c16b1a4cee1bec2abe86ff7e17
Umair Mirza <umair_hp@hotmail.com>
2022-05-15 22:14:57 +0500

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 import MainWindow from './components/MainWindow';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-      <>
+    <>
+      <Router>
         <NavBar />
         <MainWindow />
-      </>
+      </Router>
+    </>
   );
 }
 
