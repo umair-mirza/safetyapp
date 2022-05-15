5d5581f93cf6efd7b1f4d15a82fff904825a5040
Umair Mirza <umair_hp@hotmail.com>
2022-05-15 22:09:40 +0500

    added Home component

--- src/components/MainWindow.js
+++ src/components/MainWindow.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import Home from '../pages/Home';
 import AuditInspection from '../pages/AuditInspection';
 import IncidentManagement from '../pages/IncidentManagement';
 import LearningManagement from '../pages/LearningManagement';
